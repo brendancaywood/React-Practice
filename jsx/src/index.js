c68ffd556b935bd9d30754416d0e509da3c4c0c6
brendancaywood <brendan.m.caywood@gmail.com>
2018-11-26 17:07:58 -0500

    moving first proj in

---
+++ jsx/src/index.js
@@ -0,0 +1,20 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a react component
+const App = () =>{
+  return (
+    <div>
+    <label class = 'label' for="name">Enter Name:</label>
+    <input id ='name' type = 'text'/>
+    <button style={{backgroundColor: 'yellow', color: 'blue'}}>Submit</button>
+    </div>
+  );
+};
+
+//Take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
